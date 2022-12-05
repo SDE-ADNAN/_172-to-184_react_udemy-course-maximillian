a90ae6353be19960457f6455d08a4680ca1e949c
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-12-05 17:21:58 +0530

    added trhe code for video 178

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     // default fetch method is GET
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
@@ -19,15 +21,26 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
+  let content = <p>Found no movies.</p>;
+
+  // if movies is not empty then we will show the movies
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  // if isLoading is true then we will show a loading... text
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        <MoviesList movies={movies} />
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
