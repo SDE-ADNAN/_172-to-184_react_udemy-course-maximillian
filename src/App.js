0c53fe22ad36a1196d21c8269b1775eae322e644
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-12-05 17:42:59 +0530

    added the code for video 179

--- src/App.js
+++ src/App.js
@@ -6,21 +6,35 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
 
   async function fetchMoviesHandler() {
     setIsLoading(true);
+    setError(null);
     // default fetch method is GET
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      // we must check if the response is ok before we parse it or it will throw an error from that block
+      // instead of our thrown error.
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   }
   let content = <p>Found no movies.</p>;
@@ -33,6 +47,10 @@ function App() {
   // if isLoading is true then we will show a loading... text
   if (isLoading) {
     content = <p>Loading...</p>;
+  }
+
+  if (error && !isLoading) {
+    content = <p>{error}</p>;
   }
 
   return (
